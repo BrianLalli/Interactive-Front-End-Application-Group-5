4ad2397af5d5842b8f39faa8c2886fcb0cc3ce29
sl00p <lev@merka.us>
2022-07-07 12:40:54 -0700

    initial API functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,52 @@
-fetch ('https://api.open.fec.gov/developers/#/financial/get_committee__committee_id__reports_') ;
+
+var searchTerm = "smith" ;
+
+function getApi() {
+  var requestURL= ("https://api.propublica.org/campaign-finance/%7Bversion%7D/")
+  
+
+
+     
+
+  fetch (requestURL,{
+    headers :  {
+      "X-API-Key": "E6u8viCVj13rNseXzDKAbmffq8AfGmvQgHmXl687"
+      
+    }
+  })
+
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data){
+    console.log(data)
+  
+}) 
+}
+
+getApi()
+
+console.log("---------------")
+function getApi2() {
+var requestURL= ("https://api.open.fec.gov/v1/names/candidates/?q=" + searchTerm + "&api_key=XOqxKwdKYFd2FoLmHqrB69B1wFom8jVX9phrLv2D")
+fetch (requestURL)
+.then(function (response) {
+     
+      return response.json();
+    })
+    .then(function (data){
+      console.log(data)
+
+})
+    }
+
+
+
+
+getApi2()
+console.log ('wellwellwell') ;
+
+
+
+
 
