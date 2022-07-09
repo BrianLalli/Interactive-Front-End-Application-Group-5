d3091df9081f93c51db611e3364f7d721820a832
sl00p <lev@merka.us>
2022-07-09 11:24:33 -0700

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,42 @@
-
-var searchTerm = "smith" ;
+var searchTerm = "jones";
 
 function getApi() {
-  var requestURL= ("https://api.propublica.org/campaign-finance/%7Bversion%7D/")
-  
+  var requestURL =
+    "https://salty-mountain-68764.herokuapp.com/https://api.propublica.org/campaign-finance/v1/2016/candidates/search.json?query=Wilson";
 
-
-     
-
-  fetch (requestURL,{
-    headers :  {
-      "X-API-Key": "E6u8viCVj13rNseXzDKAbmffq8AfGmvQgHmXl687"
-      
-    }
+  fetch(requestURL, {
+    method: "GET",
+    headers: {
+      "X-API-KEY": "E6u8viCVj13rNseXzDKAbmffq8AfGmvQgHmXl687",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
   })
-
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data){
-    console.log(data)
-  
-}) 
-}
-
-getApi()
-
-console.log("---------------")
-function getApi2() {
-var requestURL= ("https://api.open.fec.gov/v1/names/candidates/?q=" + searchTerm + "&api_key=XOqxKwdKYFd2FoLmHqrB69B1wFom8jVX9phrLv2D")
-fetch (requestURL)
-.then(function (response) {
-     
+    .then(function (response) {
+      console.log(response);
       return response.json();
     })
-    .then(function (data){
-      console.log(data)
+    .then(function (data) {
+      console.log(data);
+    });
+}
 
-})
-    }
-
-
-
-
-getApi2()
-console.log ('wellwellwell') ;
+getApi();
 
 
+function getApi2() {
+  var requestURL =
+    "https://api.open.fec.gov/v1/names/candidates/?q=" +
+    searchTerm +
+    "&api_key=XOqxKwdKYFd2FoLmHqrB69B1wFom8jVX9phrLv2D";
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
 
-
-
+getApi2();
+console.log("wellwellwell");
